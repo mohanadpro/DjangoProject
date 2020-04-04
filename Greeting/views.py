be63adc86966044f45cc2ba8e47d14507d297100
MohanadDahi <mohanad.13.93@gmail.com>
2020-04-04 08:16:45 +0200

    add sum function

--- Greeting/views.py
+++ Greeting/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'Home.html',{'name':'Mohanad'});
+
+
+def add(request):
+    number1=int(request.GET['num1'])
+    number2=int(request.GET['num2'])
+    res=number1+number2
+
+    return render(request,'res.html',{'res':res})
+    
