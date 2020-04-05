fb1e3a3257c9eba46d74cab03da59a6da5d868e1
MohanadDahi <mohanad.13.93@gmail.com>
2020-04-05 04:31:53 +0200

    pass object to index value

--- Travello/views.py
+++ Travello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    dest=Destination()
+    dest.name='Dubai'
+    dest.description='City that is not sleep'
+    dest.price=200
+    
+    return render(request,'index.html',{'dest':dest})
