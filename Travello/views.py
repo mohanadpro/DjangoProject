fbe43f35bedb9fdc706377bcaa3379ebc5df30e5
MohanadDahi <mohanad.13.93@gmail.com>
2020-04-05 05:23:01 +0200

    pass list of objects to index html

--- Travello/views.py
+++ Travello/views.py
@@ -5,7 +5,22 @@ from .models import Destination
 def index(request):
     dest=Destination()
     dest.name='Dubai'
-    dest.description='City that is not sleep'
+    dest.description='Khalifa tower'
     dest.price=200
+    dest.img='destination_1.jpg'
+
+
+    dest1=Destination()
+    dest1.name='Lus Anjlus'
+    dest1.description='City that is not sleep'
+    dest1.price=400
+    dest1.img='destination_2.jpg'
+
+    dest2=Destination()
+    dest2.name='Paris'
+    dest2.description='Tour Eiffel'
+    dest2.price=300
+    dest2.img='destination_3.jpg'
     
-    return render(request,'index.html',{'dest':dest})
+    dests=[dest,dest1,dest2]
+    return render(request,'index.html',{'dests':dests})
