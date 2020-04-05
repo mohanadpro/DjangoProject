4dbe9e52b055ca434545dd199ee1800b576b005c
MohanadDahi <mohanad.13.93@gmail.com>
2020-04-05 10:57:19 +0200

    create database and migrate one table

--- CourseProjectWithDjango/settings.py
+++ CourseProjectWithDjango/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # determine the name that i work with to make the migration models
+    'Travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +77,13 @@ WSGI_APPLICATION = 'CourseProjectWithDjango.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # Configure our database which is postgresql
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'Travelo',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
     }
 }
 
--- Travello/models.py
+++ Travello/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id:int
-    name:str
-    description:str
-    img:str
-    price:float
-    offer:bool
+# make the class as a model to be migrated to the database
+class Destination(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    img=models.ImageField(upload_to='pics')
+    price=models.IntegerField()
+    offer=models.BooleanField(default=False)
