2a6c7f85857d8537eb39aa90661bbfb1b54b7df0
MohanadDahi <mohanad.13.93@gmail.com>
2020-04-06 04:07:15 +0200

    create superuser and admin page for Destination model

--- Travello/admin.py
+++ Travello/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Destination
 # Register your models here.
+
+
+# in this way we can register our model to be able to more destination
+admin.site.register(Destination)
